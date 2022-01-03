4c7fa9ad66b6e7815ac7d6261338d2b532145c74
benbasty <benbasty@yahoo.fr>
2022-01-03 16:02:29 +0800

    Projects Part 2

---
+++ src/com/projects/MapAsAMethodParameter.java
@@ -0,0 +1,17 @@
+package com.projects;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapAsAMethodParameter {
+    public static void main(String[] args) {
+        Map<String, String> names = new HashMap<>();
+        names.put("1", "first");
+        names.put("2", "second");
+
+        System.out.println(returnSize(names));
+    }
+    public static int returnSize(Map<String,String> names) {
+        return names.size();
+    }
+}
